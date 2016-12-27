3bb71fa64430bbe7a22331687074291bec500591
Selmir Hasanovic <shasanovic2@etf.unsa.ba>
2016-12-27 12:04:23 +0100

    ew

--- Spirala 3/Projekat/js/loginRegistration.js
+++
@@ -1,6 +0,0 @@
-function promjena(x, y) {
-	var sakrij = document.getElementById(x);
-	sakrij.style.display = "none";
-	var prikazi = document.getElementById(y);
-	prikazi.style.display = "block";
-}
